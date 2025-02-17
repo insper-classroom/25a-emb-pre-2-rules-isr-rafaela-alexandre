ba506ad60504386817435ddab0dc6b4488f7f682
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2025-02-17 18:53:26 -0300

    primeira tentativa ex2

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,25 +6,25 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+int volatile btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
 
     // ERRO: addon IsrPrintf
-    printf("btn pressed \n");
-
+    // printf("btn pressed \n");
+    
     // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
-    }
+    // while (!pio_get(BTN_PIN_R)) {
+    //   // ERRO: addon IsrNoDelay
+    //   sleep_ms(1);
+    // }
 
     // ERRO: addon IsrPrintf
-    printf("btn released \n");
+    // printf("btn released \n");
 
     // ERRO: addon IsrNoDelay
-    sleep_ms(1);
+    // sleep_ms(1);
     btn_flag = 1;
   }
 }
@@ -37,7 +37,7 @@ int main() {
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                      &btn_callback);
 
-  volatile int capture_flag = 0;
+  int capture_flag = 0;
   int a;
   while (1) {
     if (btn_flag) {
